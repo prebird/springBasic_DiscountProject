25c4e2b82b3901addd111bd5ae8d29e4d735a3ad
yonggyujeong <yonggyu.jeong10@gmail.com>
2022-04-21 01:26:48 +0900

    DIP를 준수하도록 AppCofig를 두고 의존성 주입코드로 바굼

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,12 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository ;
+
+    // 생성자를 통해서 넣어줌 -> 이 코드에는 MemoryMemberRepository(구현)이 없음 -> DIP준수
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/test/java/hello/core/member/OrderServiceTest.java
+++ src/test/java/hello/core/member/OrderServiceTest.java
@@ -1,15 +1,24 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import hello.core.discount.Order;
 import hello.core.discount.OrderService;
 import hello.core.discount.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder(){
@@ -19,7 +28,7 @@ public class OrderServiceTest {
         memberService.join(member);
         Order order =  orderService.createOrder(memberId, "snack", 3000);
 
-        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(300);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
     }
 
 }
