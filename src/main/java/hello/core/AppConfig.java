5420b574ffd8058e0790511cbde6646be7628af0
yonggyujeong <yonggyu.jeong10@gmail.com>
2022-04-24 00:04:35 +0900

    순수 자바코드 프로젝트를 스프링 프로젝트로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,21 +1,34 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.OrderService;
-import hello.core.discount.OrderServiceImpl;
+import hello.core.discount.*;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+
+@Configuration
 public class AppConfig {
 
-    // 생성자를 통해 주입한다고 해서 생성자 중입
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+    @Bean
+    public MemberService memberService(){                   // 멤버 서비스는
+        return new MemberServiceImpl(memberRepository());  //서비스임플 사용함
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    @Bean
+    public MemberRepository memberRepository(){             // 리포지토리는
+        return new MemoryMemberRepository();                // 메모리
+    }
+
+    @Bean
+    public OrderService orderService(){                                 // 오더 서비스는
+        return new OrderServiceImpl(memberRepository(), discountPolicy()); // impl
+    }
+    
+    @Bean
+    public DiscountPolicy discountPolicy(){  //할인율은
+        return new RateDiscountPolicy();                                         // 변경가능 할인율
     }
 }
